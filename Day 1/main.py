f1b41ba6c7db0a5754f7ebf6a0910b93f7ee9e95
Nikolay Ostrovsky <ostrovsky.nk@gmail.com>
2022-01-22 15:47:22 +0200

    day 1 - confirm

---
+++ Day 1/main.py
@@ -0,0 +1,76 @@
+#Вывод строк
+#Printing
+
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this:")
+# print("print('what to print')")
+
+
+#Манипуляции со строками
+#String manipulation
+
+# print("Hello world!\nHello world!\nHello world!")
+# print("Hello" + " " + "Nikolay")
+
+
+#Исправление строк
+#Debugging Practice
+
+# print("Day 1 - String Manipulation")
+# print("String Concatenation is done with the" + " sign.")
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+
+#Конкатенация строк с использованием input()
+#Strings concatenation with use input()
+
+# print("Hello " + input("What's your name?") + "!")
+
+#Посчитать кол-во символов в имени
+#Count sumbols in name
+
+# print(len(input("What's your name?")))
+
+
+#Переменные
+#Variables
+
+# name = input("What's your name?\n")
+# print(name)
+
+# name = "Nikolay"
+# print(name)
+
+# name = "Angela"
+# print(name)
+
+# name = input("What's your name?\n")
+# length = len(name)
+# print(length)
+
+
+#Практика по перменным
+#Practice variables
+
+# a = input("a:")
+# b = input("b:")
+
+# tempA = a
+# tempB = b
+
+# a = tempB
+# b = tempA
+
+# print("a = " + a)
+# print("b = " + b)
+
+
+#ДЕНЬ 1 ГЕНЕРАТОР ИМЕНИ ГРУППЫ
+#DAY 1 BAND NAME GENERATOR
+
+print("Welcone to the Band Name Generator")
+
+cityName = input("What's name of the city you grew up in?\n")
+petName = input("What's your pet's name?\n")
+
+print("You band name could be " + cityName + " " + petName)
